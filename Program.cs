d4dafa7408e5a0c10542f0374b275db62ec9f6cf
dabhade904 <dabhade904@gmail.com>
2022-08-19 14:12:58 +0530

    Added :Initialise project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListGeneric
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Message");
+           
+        }
+    }
+}
