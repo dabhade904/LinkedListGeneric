6c16e76675a631cbc83210ae6ec3c8b3c37bfcad
dabhade904 <dabhade904@gmail.com>
2022-08-19 14:17:52 +0530

    Added : function to create linked list

--- Node.cs
+++ Node.cs
@@ -8,6 +8,12 @@ namespace LinkedListGeneric
 {
     public class Node<T>
     {
-      
+        public T data;
+        public Node <T>next;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace LinkedListGeneric
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome Message");
-           
+            LinkedList<int> list = new LinkedList<int>();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
